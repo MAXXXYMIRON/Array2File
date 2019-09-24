13bdeb985ef0349b1f5a566f1b99eea7fe8223dd
Maks <klavioturasucka2@gmail.com>
2019-09-24 22:56:12 +0900

    narmalJob

--- Array2File/mainwindow.cpp
+++ Array2File/mainwindow.cpp
@@ -18,7 +18,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->tableWidget->setRowCount(1);
+    ui->TWFirstArr->setRowCount(1);
+    ui->TWSecondArr->setRowCount(1);
+    ui->TWFileArr->setRowCount(1);
 }
 
 MainWindow::~MainWindow()
@@ -40,35 +42,39 @@ void MainWindow::on_ButtonStart_clicked()
 
     //Инциализация массива
     srand(time(NULL));
-    n = ui->spinBoxValue->value();
+
+    n = ui->spinBoxValue->value();//Берём значение из SpinBox'a
     int *array = MakeArray(n);
 
+    array = IncArray(array, n,
+        [](int x, int y)->int {return rand() % 10;});
 
-    ui->tableWidget->setColumnCount(n);
 
-
+    ui->TWFirstArr->setColumnCount(n);
+    ui->TWSecondArr->setColumnCount(n);
+    ui->TWFileArr->setColumnCount(n);
     QTableWidgetItem *cell;
 
     for(int i = 0; i < n; i++)
     {
-        if(ui->tableWidget->item(0,i) == nullptr)
+        if(ui->TWFirstArr->item(0,i) == nullptr)
         {
             cell = new QTableWidgetItem();
-            ui->tableWidget->setItem(0, i, cell);
+            ui->TWFirstArr->setItem(0, i, cell);
+            cell = new QTableWidgetItem();
+            ui->TWSecondArr->setItem(0, i, cell);
+            cell = new QTableWidgetItem();
+            ui->TWFileArr->setItem(0, i, cell);
         }
     }
 
-    array = IncArray(array, n,
-        [](int x, int y)->int {return rand();});
-
+    //Вывод массива
     OutArrayInTextEdin(array, n,
-                       [=](int Arr, int x)
-                       {
-                            for(int i = 0; i < n; i++)
-                            {
-                                ui->tableWidget->item(0, i)->setText(QString::number(Arr));
-                            }
-                       });
+       [=](int Arr, int x)
+       {
+            ui->TWFirstArr->item(0, x)->setText(QString::number(Arr));
+       });
+
 
 
     //Работа с файлом
@@ -83,16 +89,14 @@ void MainWindow::on_ButtonStart_clicked()
 
         //Изменяем массив
         array = IncArray(array, n,
-            [](int x, int y)->int {return rand();});
+            [](int x, int y)->int {return rand() % 10;});
 
         OutArrayInTextEdin(array, n,
-                           [=](int Arr, int i)
-                           {
-                                for(int i = 0; i < n; i++)
-                                {
-                                    ui->tableWidget->item(0, i)->setText(QString::number(Arr));
-                                }
-                           });
+           [=](int Arr, int x)
+           {
+                ui->TWSecondArr->item(0, x)->setText(QString::number(Arr));
+           });
+
 
 
         //Считываем массив из файла
@@ -102,32 +106,28 @@ void MainWindow::on_ButtonStart_clicked()
         array = fileToArray(ArrFil);
 
         OutArrayInTextEdin(array, n,
-                           [=](int Arr, int i)
-                           {
-                                for(int i = 0; i < n; i++)
-                                {
-                                    ui->tableWidget->item(0, i)->setText(QString::number(Arr));
-                                }
-                           });
+           [=](int Arr, int x)
+           {
+                ui->TWFileArr->item(0, x)->setText(QString::number(Arr));
+           });
 
         ArrFil.close();
     }
+
+
     catch (int e)
     {
         if (e == 1)
         {
             QMessageBox::warning(this, "Внимание", "Ошибка при окрытии файла на запись");
-            close();
         }
         else if (e == 2)
         {
             QMessageBox::warning(this, "Внимание", "Ошибка при окрытии файла на чтение");
-            close();
         }
         else
         {
             QMessageBox::warning(this, "Внимание", "Файл пуст");
-            close();
         }
     }
 
